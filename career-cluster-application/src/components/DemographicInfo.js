82c82b9859f37ef2d688e00753eb75b00064b5f4
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-16 13:55:59 -0500

    Added return for the HTML

--- career-cluster-application/src/components/DemographicInfo.js
+++ career-cluster-application/src/components/DemographicInfo.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ClusterFunctions from '../ClusterFunctions';
-import { CareerClusterMap } from '../ClusterFunctions.js'
-import { GetClusterNameByID } from '../ClusterFunctions.js'
+
 
 
 
 
 const DemographicInfo = () => {
+  return (
     <div>
       <h1>Haiii :3</h1>
     </div>
+  );
 }
 
 export default DemographicInfo;
