4e8ab1aad691c2ac1418c058154dafdc6fb4f576
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-16 13:55:43 -0500

    Router fix

--- career-cluster-application/src/components/App.js
+++ career-cluster-application/src/components/App.js
@@ -1,20 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 // import { Link, useNavigate } from 'react-router-dom';
-import Cluster from './Cluster.js'
-import DemographicInfo from './DemographicInfo.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Cluster from './Cluster'
+import DemographicInfo from './DemographicInfo';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
-import { CareerClusterMap } from '../ClusterFunctions.js'
-import { GetClusterNameByID } from '../ClusterFunctions.js'
-import ClusterFunctions from '../ClusterFunctions.js'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<DemographicInfo />} />
         <Route path="/cluster" element={<Cluster />} />
       </Routes>
     </Router>
