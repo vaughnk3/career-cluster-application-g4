84cef8325e53e37708e6698a649d12bf112d830d
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-17 17:15:07 -0500

    Just trying to get image to load.

--- career-cluster-application/src/components/pageTemplate/TopLeftLogo.js
+++ career-cluster-application/src/components/pageTemplate/TopLeftLogo.js
@@ -2,7 +2,7 @@ import React from "react";
 import './TopLeftLogo.css';
 
 const TopLeftLogo = () => {
-    return <img src="./YorkLogo.png" alt="Your Image" class="top-left-image"></img>
+    return <img src="./YorkLogo.png" alt="Top Left Image" className="top-left-image"></img>
 }
 
 export default TopLeftLogo;
