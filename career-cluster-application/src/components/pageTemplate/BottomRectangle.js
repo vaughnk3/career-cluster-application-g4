fd774a2d663ef45f11ad0b99e35bc1ea0a9a989b
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-17 13:16:40 -0500

    Made the component for the rectangle

---
+++ career-cluster-application/src/components/pageTemplate/BottomRectangle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BottomRectangle.css';
+
+const BottomRectangle = () => {
+  return <div className="bottom-rectangle"></div>;
+};
+
+export default BottomRectangle;
