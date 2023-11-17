07e25d0d9b551897754e03259652ee465466e03d
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-17 15:03:42 -0500

    Created component for the demographic information box.

---
+++ career-cluster-application/src/components/DemographicBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DemographicBox.css';
+
+const DemographicBox = () => {
+  return ( 
+    <div className="demographic-box">
+        <button class="submit-button">Submit</button>
+    </div>
+  );
+};
+
+export default DemographicBox;
