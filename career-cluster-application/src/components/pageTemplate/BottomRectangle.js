81087e75d872cd6e7b4dc20fa07dfd3b1d1e32c3
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-17 14:20:18 -0500

    Component for the rectangle at bottom of page

--- career-cluster-application/src/components/pageTemplate/BottomRectangle.js
+++ career-cluster-application/src/components/pageTemplate/BottomRectangle.js
@@ -2,7 +2,19 @@ import React from 'react';
 import './BottomRectangle.css';
 
 const BottomRectangle = () => {
-  return <div className="bottom-rectangle"></div>;
+    return (
+        <div className="bottom-rectangle">
+            <div className="text-container"></div>
+                <div className="blue-text">GET IN TOUCH</div>
+                <div className="white-text">York County Regional Chamber of Commerce</div>
+                <div className="white-text">116 E Main St. / PO Box 590</div>
+                <div className="white-text">Rock Hill, SC, 29731</div>
+                <div className="white-text">803-324-7500</div>
+            <div/>
+          <img src="./YCRCLogo.png" alt="Left Image" className="image left-image" />
+          <img src="./Accredit.png" alt="Right Image" className="image right-image" />
+        </div>
+      );
 };
 
 export default BottomRectangle;
