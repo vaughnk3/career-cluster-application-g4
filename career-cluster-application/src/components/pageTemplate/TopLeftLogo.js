7657925dc314c052aacb340e036863ff09f0bedc
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-17 14:20:35 -0500

    Component for image in top left of dem. page

---
+++ career-cluster-application/src/components/pageTemplate/TopLeftLogo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './TopLeftLogo.css';
+
+const TopLeftLogo = () => {
+    return <img src="./YorkLogo.png" alt="Your Image" class="top-left-image"></img>
+}
+
+export default TopLeftLogo;
