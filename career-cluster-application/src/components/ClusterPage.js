d9bd70d4a0e1ad23ea3ac9183ed1f094f02c0ee7
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-18 14:04:03 -0500

    Example test array.

--- career-cluster-application/src/components/ClusterPage.js
+++ career-cluster-application/src/components/ClusterPage.js
@@ -3,10 +3,29 @@ import Cluster from './Cluster.js'
 import BottomRectangle from "./pageTemplate/BottomRectangle.js";
 import UserIcon from "./pageTemplate/UserIcon.js";
 import TopRectangle from "./pageTemplate/TopRectangle.js";
-
+import { CareerClusterMap } from '../ClusterFunctions.js';
+import { GetClusterNameByID } from '../ClusterFunctions.js';
 import './ClusterPage.css';
 
 const ClusterPage = () => {
+
+    //This is one idea I had to get all the clusters without null ID's.  
+    //In this case, we could loop through this array which stores the valid ID's to get the clusters we want
+    //using the methods in ClusterFunctions.js
+    let ValidClusterArray = [];
+    
+    for (let i = 1; i < CareerClusterMap.size; i++)
+    {
+        if (GetClusterNameByID(CareerClusterMap, i) != 0) 
+        {
+            ValidClusterArray.push(i);
+        }
+
+    }
+    console.log("Valid ARRAY   " , ValidClusterArray)
+    //Now we would know what clusters are valid to be used.  Hopefully there is some way to loop through the ID's 
+    // In here and pass them to 'Cluster.js' and get returned the cluster element with the name, image corresponding to the passed ID
+
     return (
     <div>
         <TopRectangle/>
