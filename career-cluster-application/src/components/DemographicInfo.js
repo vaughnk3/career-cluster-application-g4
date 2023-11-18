fbfab985f5f1280dd8b5c2ce2891e259771100e4
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-18 09:38:05 -0500

    Added usericon component to demographic page

--- career-cluster-application/src/components/DemographicInfo.js
+++ career-cluster-application/src/components/DemographicInfo.js
@@ -2,12 +2,15 @@ import React from 'react';
 import BottomRectangle from './pageTemplate/BottomRectangle.js';
 import TopLeftLogo from './pageTemplate/TopLeftLogo.js'
 import DemographicBox from './DemographicBox.js';
+import UserIcon from './pageTemplate/UserIcon.js';
+
 
 
 const DemographicInfo = () => {
   return (
     <div>
       <TopLeftLogo/>
+      <UserIcon/>
       <BottomRectangle/>
       <DemographicBox/>
     </div>
