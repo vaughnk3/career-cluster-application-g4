fdd4d82f886264122cd549bf7f6d1236a79b6a53
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-18 10:10:23 -0500

    Changed route element to ClusterPage

--- career-cluster-application/src/components/App.js
+++ career-cluster-application/src/components/App.js
@@ -4,13 +4,14 @@ import Cluster from './Cluster'
 import DemographicInfo from './DemographicInfo';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
+import ClusterPage from './ClusterPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<DemographicInfo />} />
-        <Route path="/cluster" element={<Cluster />} />
+        <Route path="/cluster" element={<ClusterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
