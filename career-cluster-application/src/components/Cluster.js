a5758cc422a64c1cdcb3b1c9c68a0d3dba89857c
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-18 14:03:35 -0500

    Attempt at this working

--- career-cluster-application/src/components/Cluster.js
+++ career-cluster-application/src/components/Cluster.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ClusterFunctions from '../ClusterFunctions';
+import ClusterFunctions, { GetClusterImageByID } from '../ClusterFunctions';
 import { CareerClusterMap } from '../ClusterFunctions.js'
 import { GetClusterNameByID } from '../ClusterFunctions.js'
-
+//import { GetClusterImageByID } from '../ClusterFunctions';
 import './Cluster.css';
 
 
@@ -10,8 +10,11 @@ const Cluster = () => {
   return (
     <div class="cluster">
       <h2>{GetClusterNameByID(CareerClusterMap, 1)}</h2>
+      <img src={ GetClusterImageByID(CareerClusterMap, 1) } alt="Cluster Picture" className="cluster-pics"></img>
     </div>
   );
 }
 
 export default Cluster;
+
+//require('./Cluster_Pictures/Mathematics.png')
