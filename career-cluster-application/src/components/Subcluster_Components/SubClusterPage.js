62b65e5c2f021d858e663a9de32a8c77e91728ce
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-19 21:00:42 -0500

    Added the subcluster page component

---
+++ career-cluster-application/src/components/Subcluster_Components/SubClusterPage.js
@@ -0,0 +1,21 @@
+import BottomRectangle from "../pageTemplate/BottomRectangle.js";
+import UserIcon from "../pageTemplate/UserIcon.js";
+import TopRectangle from "../pageTemplate/TopRectangle.js";
+import { CareerClusterMap } from '../../ClusterFunctions.js'
+
+
+const SubClusterPage = () => {
+    return (
+        <div>
+            <TopRectangle/>
+            <UserIcon/>
+            <h1> Sub Cluster Page </h1>
+
+
+            <BottomRectangle/>
+        </div>
+    )
+}
+
+
+export default SubClusterPage;
