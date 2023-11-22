071d422b23f462f601791586f474d5087c9dee6d
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-21 21:40:55 -0500

    Display all sub cluster components after doing proper checks

--- career-cluster-application/src/components/Subcluster_Components/SubClusterPage.js
+++ career-cluster-application/src/components/Subcluster_Components/SubClusterPage.js
@@ -3,11 +3,46 @@ import UserIcon from "../pageTemplate/UserIcon.js";
 import TopRectangle from "../pageTemplate/TopRectangle.js";
 import { CareerClusterMap } from '../../ClusterFunctions.js';
 import { GetClusterNameByID } from "../../ClusterFunctions.js";
+import { GetFieldsByID } from "../../ClusterFunctions.js";
+import { GetFieldNameByIDS } from "../../ClusterFunctions.js";
 import { useLocation } from 'react-router-dom';
+import SubCluster from "./SubCluster.js";
+import FinalCluster  from "../../ClusterFunctions.js";
 
 const SubClusterPage = ( {} ) => {
     // const location = useLocation(); 
     // const { clusterID } = location.state || {}; Failed Attempt at sending ID over -- correspondings to attempt with <Link> in Cluster.js
+    //Check to see what final field is not null, this index is then the cluster ID
+    let ClusterID = null;
+    for (let i = 1; i <= CareerClusterMap.size; i++) 
+    {
+        if (CareerClusterMap.get(i).final != null) 
+        {
+            ClusterID = i;
+        }
+    }
+
+    console.log("SUBCLUSTER TEST   ", ClusterID);
+
+    
+    //Get all the good subcluster "ids" to know which ones to list.
+    let SubClusterArray = GetFieldsByID (CareerClusterMap, ClusterID);
+    let GoodSubClusterArary = [];
+    for (let i = 0; i < SubClusterArray.length; i++)
+    {
+        if (GetFieldNameByIDS(CareerClusterMap, ClusterID, i) != null)
+        {
+            GoodSubClusterArary.push(i);
+        }
+    }
+    
+    //Now store the good components for subclusters
+    let subComponentArray = [];
+    for (let j = 0; j < GoodSubClusterArary.length; j++)
+    {
+        subComponentArray.push(<SubCluster ID={ClusterID} subID={GoodSubClusterArary[j]} />)
+    }
+    
 
     return (
         <div>
@@ -16,7 +51,8 @@ const SubClusterPage = ( {} ) => {
             <h1> Sub Cluster Page </h1>
             <h1> Haiiiii </h1>
             {/* <h1 id="test">{clusterID}</h1> */}
-
+            <p> {ClusterID} </p>
+            <ul> {subComponentArray} </ul>
             <BottomRectangle/>
         </div>
     )
