510c051150b37fb33c4277cae96aa0372ba15b03
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-21 21:40:36 -0500

    Component definition for sub cluster

---
+++ career-cluster-application/src/components/Subcluster_Components/SubCluster.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ClusterFunctions, { GetClusterImageByID } from '../../ClusterFunctions';
+import FinalCluster, { CareerClusterMap } from '../../ClusterFunctions.js'
+import { Link } from 'react-router-dom';
+import { GetClusterNameByID } from '../../ClusterFunctions.js'
+import { GetFieldNameByIDS } from "../../ClusterFunctions";
+import { GetFieldImageByIDS } from "../../ClusterFunctions";
+
+const SubCluster = ( {ID, subID} ) => {
+    return (
+        <div className="subcluster"> 
+            <h2> { GetFieldNameByIDS(CareerClusterMap, ID, subID) } </h2>
+            <h3> { GetFieldImageByIDS(CareerClusterMap, ID, subID) } </h3>
+        </div>
+    );
+};
+
+export default SubCluster
