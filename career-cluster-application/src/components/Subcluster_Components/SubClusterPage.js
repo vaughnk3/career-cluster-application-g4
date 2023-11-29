f5be1f61927686ef424e136dc59a263bad5a39a2
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2023-11-29 01:09:03 -0500

    Added subcluster click handler

--- career-cluster-application/src/components/Subcluster_Components/SubClusterPage.js
+++ career-cluster-application/src/components/Subcluster_Components/SubClusterPage.js
@@ -1,7 +1,7 @@
 import BottomRectangle from "../pageTemplate/BottomRectangle.js";
 import UserIcon from "../pageTemplate/UserIcon.js";
 import TopRectangle from "../pageTemplate/TopRectangle.js";
-import { CareerClusterMap } from '../../ClusterFunctions.js';
+import { CareerClusterMap, SetFieldFinalToNull } from '../../ClusterFunctions.js';
 import { GetClusterNameByID } from "../../ClusterFunctions.js";
 import { GetFieldsByID } from "../../ClusterFunctions.js";
 import { GetFieldNameByIDS } from "../../ClusterFunctions.js";
@@ -9,8 +9,45 @@ import { useLocation } from 'react-router-dom';
 import SubCluster from "./SubCluster.js";
 import FinalCluster  from "../../ClusterFunctions.js";
 import './SubClusterPage.css'
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import Cluster from "../Cluster.js";
+
+
+
 
 const SubClusterPage = ( {} ) => {
+    const navigate = useNavigate();
+    const [selectedCluster, setSelectedCluster] = useState(null);
+
+    const handleClusterClick = (SubID) => {
+        console.log(SubID)
+        navigate('/cluster/subcluster/subclusterinfo')
+
+        let ClusterID = null;
+        //Grab the Cluster ID
+        for (let i = 1; i <= CareerClusterMap.size; i++) 
+        {
+            if (CareerClusterMap.get(i).final != null) 
+            {
+                ClusterID = i;
+            }
+        }
+        //console.log("SUBFIELD ID = ", SubID, "NOW CLUSTER  ", ClusterID);
+
+        //Set all subfield finals to null (incase of a clickback)
+        for (let i = 0; i < CareerClusterMap.get(ClusterID).SubClusters.length; i++)
+        {
+            SetFieldFinalToNull(CareerClusterMap, ClusterID, i);
+        }
+        
+        //Now, update the fieldFinal data with the SubId 
+        CareerClusterMap.get(ClusterID).SubClusters[SubID].fieldFinal = SubID;
+
+    }
+
+
+
     // const location = useLocation(); 
     // const { clusterID } = location.state || {}; Failed Attempt at sending ID over -- correspondings to attempt with <Link> in Cluster.js
     //Check to see what final field is not null, this index is then the cluster ID
@@ -42,7 +79,7 @@ const SubClusterPage = ( {} ) => {
     let subComponentArray = [];
     for (let j = 0; j < GoodSubClusterArary.length; j++)
     {
-        subComponentArray.push(<SubCluster ID={ClusterID} subID={GoodSubClusterArary[j]} />)
+        subComponentArray.push(<SubCluster ID={ClusterID} subID={GoodSubClusterArary[j]} onClick={handleClusterClick} />)
     }
     
 
